2e904517b5c821aa4d41ff3777c49a8e363fc0ed
APJishnu <apjishnu23@gmail.com>
2024-11-21 16:12:56 +0530

    deleted yarn lock

--- src/module/login/components/LoginForm/LoginForm.tsx
+++ src/module/login/components/LoginForm/LoginForm.tsx
@@ -5,7 +5,15 @@ const LoginForm = () => {
   return (
     <div className={styles.LoginForm}>
       <div>
-        <ModalComponent isVisible={true} title={"Logout?"} content={<div>You are about to logout . Are you sure you want to logout</div>} primaryButtonLabel={"cancel"} secondaryButtonLabel={"logout"}/>
+        <ModalComponent
+          isVisible={true}
+          title={"Logout?"}
+          content={
+            <div>You are about to logout . Are you sure you want to logout</div>
+          }
+          primaryButtonLabel={"cancel"}
+          secondaryButtonLabel={"logout"}
+        />
       </div>
     </div>
   );
