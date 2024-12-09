f57f388c5ddba3fbb004395194e69d23a1ceb8e2
Yadhukrishnan <yadhukrishnan2030@gmail.com>
2024-12-09 14:23:13 +0530

    Approval center Employee listing

---
+++ src/module/approval-center/components/employee-list-tab/employee-list/employee-list.tsx
@@ -0,0 +1,34 @@
+import { TeamMember } from "@/module/approval-center/services/all-time-sheet-services";
+import React from "react";
+import EmployeeCard from "./employee-card/employee-card";
+import styles from "./employee-list.module.scss";
+import { useRouter } from "next/navigation";
+
+
+interface EmployeeListProps {
+  employeeList: TeamMember[];
+}
+const EmployeeList: React.FC<EmployeeListProps> = ({ employeeList }) => {
+  const router = useRouter();
+  const handleRouting = (id: string) => {
+    console.log(id);
+    router.push(`/time-sheet/review-timesheet/${id}`);
+  };
+  return (
+    <div className={styles.employeeListWrapper}>
+      {employeeList.map((employee) => {
+        return (
+          <EmployeeCard
+            name={employee.name}
+            avatar={employee.avatar}
+            onClickFunction={() => {
+              handleRouting(employee.id);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default EmployeeList;
