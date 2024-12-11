3b257a1057de4041c5995a5e8ac19b2d3cf25772
akhilvijayakumar <vpaqil3@gmail.com>
2024-12-11 11:04:04 +0530

    modified module header and header wrapper

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import NavBar from "@/themes/components/nav-bar/nav-bar";
 import './global.scss'
 import ReduxProvider from "@/redux/redux-provider";
-import ModuleHeaderWrapper from "@/themes/components/module-header-wrapper-new/module-header-wrapper.module";
+import ModuleHeaderWrapper from "@/themes/components/module-header-wrapper/module-header-wrapper";
 
 export default function Layout({
   children,
