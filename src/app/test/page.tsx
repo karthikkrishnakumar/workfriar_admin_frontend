3c0a718a7663bcddd98666f8bc6e8b18995200af
Yadhukrishnan <yadhukrishnan2030@gmail.com>
2025-01-13 11:01:42 +0530

    Build error fixed

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import EmployeeCard from "@/module/approval-center/components/employee-list-tab/employee-list/employee-card/employee-card";
-import StatusTag from "@/module/time-sheet/components/status-tag/status-tag";
-import MultipleBodyTable, { BodyData } from "@/themes/components/multiple-body-table/multiple-body-table";
-import React, { useRef, useState } from "react";
+import React from "react";
 
 export default function Page() {
 
@@ -10,7 +7,6 @@ export default function Page() {
 
   return (
     <div>
-      <StatusTag status="rejected" />
     </div>
   );
 }
