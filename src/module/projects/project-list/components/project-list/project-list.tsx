aad8a139085fed43d80666e5604ff23e13112bd5
KeerthySudev <keerthysudev33@gmail.com>
2025-01-20 14:28:57 +0530

    update projects - error fixed

--- src/module/projects/project-list/components/project-list/project-list.tsx
+++ src/module/projects/project-list/components/project-list/project-list.tsx
@@ -98,11 +98,13 @@ const ProjectList: React.FC = () => {
    * @param {Record<string, any>} values - The updated values for the project
    */
   const handleEditProjectSubmit = async (values: Record<string, any>) => {
+    console.log(values)
     const payload = {
       ...values,
-      categories: values.categories.map((cat: any) => cat.id),
+      categories: values.categories.map((cat: any) => cat),
       project_logo: values.project_logo,
     };
+    console.log(payload);
     try {
       const response = await updateProject(selectedId, payload);
       if (response.status) {
